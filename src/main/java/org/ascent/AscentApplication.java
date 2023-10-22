c30d11d58a33edbeda3c43db8a62362c216a71ab
ivasibi <ivansibilla@outlook.com>
2023-10-22 10:40:12 +0200

    initial commit

---
+++ src/main/java/org/ascent/AscentApplication.java
@@ -0,0 +1,12 @@
+package org.ascent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AscentApplication.class, args);
+    }
+}
