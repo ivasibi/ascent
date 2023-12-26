2103b0680b75142ce24d7aee2b84538668d452dd
ivasibi <ivansibilla@outlook.com>
2023-12-26 15:24:05 +0100

    corrected annotation

--- src/main/java/org/ascent/configurations/BCryptConfiguration.java
+++ src/main/java/org/ascent/configurations/BCryptConfiguration.java
@@ -1,10 +1,10 @@
 package org.ascent.configurations;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 public class BCryptConfiguration {
 
     @Bean
